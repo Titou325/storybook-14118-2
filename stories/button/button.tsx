37b3b8b2269c4da6f2dedbd7e7b18b88ef7d4d3d
Titouan Launay <titouan.launay@lleedpartners.com>
2021-03-06 13:34:09 +0100

    Adds enum types

--- stories/button/button.tsx
+++ stories/button/button.tsx
@@ -7,13 +7,17 @@ export type IButtonProps = {
      * Simple click handler
      */
     onClick?: () => void;
+    /** Example enum prop */
+    suffix: "a" | "b" | "c";
+    /** Example optional enum prop */
+    prefix?: "a" | "b" | "c";
 };
 
 /**
  * The world's most _basic_ button
  */
-export const Button: FC<IButtonProps> = ({ children = "Button", onClick }: IButtonProps) => (
+export const Button: FC<IButtonProps> = ({ children = "Button", suffix = "a", onClick }: IButtonProps) => (
     <button onClick={onClick} type="button">
-        {children}
+        {children} - {suffix}
     </button>
 );
