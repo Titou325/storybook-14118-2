a7142c691d6c7c43021b4c70e2c1886aa81a36f5
Titouan Launay <titouan.launay@lleedpartners.com>
2021-03-06 13:41:23 +0100

    Simplifies definitions

--- stories/button/button.tsx
+++ stories/button/button.tsx
@@ -1,16 +1,21 @@
 import React, { FC, ReactNode } from "react";
 
 export type ButtonProps = {
+    /** Suffix to apply to the button */
     suffix?: string;
+    /** Child elements */
     children: ReactNode;
+    /** OnClick event handler */
     onClick?: () => void;
+    /** Optional prefix */
+    prefix?: "a" | "b"
 }
 
 /**
  * The world's most _basic_ button
  */
-export const Button = ({ children = "Button", suffix = "a", onClick }: ButtonProps): JSX.Element => (
+export const Button = ({ children = "Button", prefix, suffix = "a", onClick }: ButtonProps): JSX.Element => (
     <button onClick={onClick} type="button">
-        {children} - {suffix}
+        {prefix}{children} - {suffix}
     </button>
 );
